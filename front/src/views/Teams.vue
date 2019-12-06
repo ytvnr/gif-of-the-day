6e9d28c309a4305203687b50d4f14a2c21a66d41
Yann Tavernier <yann.tavernier@qima.com>
2019-12-06 13:55:47 +0100

    feat: display teams collection in Teams.vue 🤼‍

---
+++ front/src/views/Teams.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="teams">
+    <h1>This is the list of the teams</h1>
+    {{ JSON.stringify(teams) }}
+  </div>
+</template>
+
+<script>
+
+  import firebase from "firebase";
+
+  const db = firebase
+  .initializeApp(require('../firebaseConfig.json'))
+  .firestore();
+
+  export default {
+      name: "teams",
+      firestore: {
+          fireTeams: db.collection('teams')
+      },
+      data() {
+          return {name: "test"};
+      },
+      created() {
+          this.getTeams();
+      },
+      computed: {
+          teams() {
+              return this.fireTeams;
+          }
+      },
+      methods: {
+          getTeams() {
+              console.log("Attempt to get teams: " + this.name);
+              console.log(this.teams);
+          }
+      }
+  }
+</script>
