f8f8c5636ad55082aa8ae8ce8e824d5dff688eeb
Aurélien Loyer <aur.loy@gmail.com>
2019-12-09 14:35:49 +0100

    feat: add first verison of navbar and teams layout 📏

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/teams">Teams</router-link>
-    </div>
-    <router-view/>
+    <god-navbar></god-navbar>
+    <router-view />
   </div>
 </template>
 
+<script>
+
+import NavBar from '@/components/NavBar'
+
+export default {
+  components: {
+    'god-navbar': NavBar, 
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/">Home</router-link>
+        <router-link class="navbar-item" to="/teams">Teams</router-link>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">More</a>
+
+          <div class="navbar-dropdown">
+            <router-link class="navbar-item" to="/settings">Settings</router-link>
+            <router-link class="navbar-item" to="/settings">Settings</router-link>
+            <hr class="navbar-divider" />
+            <router-link class="navbar-item" to="/settings">Settings</router-link>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Log in</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
