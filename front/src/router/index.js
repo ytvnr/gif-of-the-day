426b0751ca8a8df1ccfde38918a9bf51bb9e436f
Yann Tavernier <47851994+ytvnr@users.noreply.github.com>
2020-01-18 19:42:43 +0100

    feat: init gif table

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -18,6 +18,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/Teams.vue')
+  },
+  {
+    path: '/gifs',
+    name: 'gifs',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Gifs.vue')
   }
 ];
 
