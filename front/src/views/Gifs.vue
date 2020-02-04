5917af6e470d27387cbf77883b5c2ad1d940e2c4
Yann Tavernier <47851994+ytvnr@users.noreply.github.com>
2020-02-04 13:03:41 +0100

    feat: use vuetify editable table (#41) ⭐️

--- front/src/views/Gifs.vue
+++ front/src/views/Gifs.vue
@@ -4,47 +4,46 @@
       <v-card>
         <v-card-text>
           <div class="gifs">
-            <h2>GIFFFFFSSSS</h2>
-            <div class="table-container">
-              <table class="table is-bordered is-striped is-narrow is-hoverable">
-                <thead>
-                  <tr>
-                    <th>Day</th>
-                    <th>Theme</th>
-                    <th>Preview</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>Monday</th>
-                    <td>Monday Theme</td>
-                    <td>Preview</td>
-                  </tr>
-                  <tr>
-                    <th>Tuesday</th>
-                    <td>Tuesday Theme</td>
-                    <td>Preview</td>
-                  </tr>
-                  <tr>
-                    <th>Wednesday</th>
-                    <td>Wednesday Theme</td>
-                    <td>Preview</td>
-                  </tr>
-                  <tr>
-                    <th>Thursday</th>
-                    <td>Thursday Theme</td>
-                    <td>Preview</td>
-                  </tr>
-                  <tr>
-                    <th>Friday</th>
-                    <td>Friday Theme</td>
-                    <td>Preview</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <h1 class="gifs-title">Gif of the day</h1>
           </div>
         </v-card-text>
+        <v-data-table
+                :headers="headers"
+                :items="gifs"
+                :disable-filtering="true"
+                :disable-pagination="true"
+                :disable-sort="true"
+                :hide-default-footer="true"
+        >
+          <template v-slot:item.theme="props">
+            <v-edit-dialog
+                    :return-value.sync="props.item.theme"
+                    large
+                    persistent
+                    @save="save"
+                    @cancel="cancel"
+                    @open="open"
+                    @close="close"
+            >
+              <div>{{ props.item.theme }}</div>
+              <template v-slot:input>
+                <div class="mt-4 title">Update Theme</div>
+              </template>
+              <template v-slot:input>
+                <v-text-field
+                        v-model="props.item.theme"
+                        label="Edit"
+                        single-line
+                        counter
+                        autofocus
+                ></v-text-field>
+              </template>
+            </v-edit-dialog>
+          </template>
+          <template v-slot:item.preview>
+            <v-btn class="ma-2" outlined color="lime">Preview</v-btn>
+          </template>
+        </v-data-table>
         <v-card-actions></v-card-actions>
       </v-card>
     </v-col>
@@ -58,6 +57,69 @@
 export default {
   name: 'gifs',
   components: {
+  },
+  data() {
+    return {
+      disableFiltering: true,
+      pagination: {},
+      headers: [
+        {
+          text: 'Day of week',
+          align: 'left',
+          sortable: false,
+          value: 'day',
+        },
+        { text: 'Gif Theme', value: 'theme' },
+        { text: 'Preview', value: 'preview' }
+      ],
+      gifs: [
+        {
+          day: 'Monday',
+          theme: 'Monday Theme',
+          preview: 'Preview',
+        },
+        {
+          day: 'Tuesday',
+          theme: 'Tuesday Theme',
+          preview: 'Preview',
+        },
+        {
+          day: 'Wednesday',
+          theme: 'Wednesday Theme',
+          preview: 'Preview',
+        },
+        {
+          day: 'Thursday',
+          theme: 'Thursday Theme',
+          preview: 'Preview',
+        },
+        {
+          day: 'Friday',
+          theme: 'Friday Theme',
+          preview: 'Preview',
+        },
+      ]
+    };
+  },
+  methods: {
+    save() {
+      console.log('Dialog saved');
+    },
+    cancel() {
+      console.log('Dialog cancelled');
+    },
+    open() {
+      console.log('Dialog opened');
+    },
+    close() {
+      console.log('Dialog closed');
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .gifs-title {
+    text-align: center;
+  }
+</style>
