9b1dc9ca94618f5b366a6c48f67f362f8d5a337e
Aurélien Loyer <3717296+T3kstiil3@users.noreply.github.com>
2020-02-06 13:31:53 +0100

    feat: restrict page access 👮‍♂️

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    #home
+  <div class="about">
+    #about
   </div>
 </template>
 
@@ -9,7 +9,7 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'about',
   components: {
   }
 }
