0b47855f467bf84411d286a19d33c807297ed18d
Aurélien Loyer <aur.loy@gmail.com>
2020-02-19 14:59:33 +0100

    fix: rework on router guards to prevent redirect on refresh 🐜

---
+++ front/src/firebase/init.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+
+const firebaseConfig = require('./config.json');
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+
+firebase.getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
+};
+
+export default firebase;
