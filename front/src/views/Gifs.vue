3a1898a9ab3768b820da6e4a62b4abbe2dcc9e9b
Aurélien Loyer <aur.loy@gmail.com>
2020-03-26 09:28:49 +0100

    feat: display gifs list 🥳

--- front/src/views/Gifs.vue
+++ front/src/views/Gifs.vue
@@ -1,21 +1,113 @@
 <template>
-  <div class="fake">
-    <img alt="fake gifs list" src="../../src/assets/pages/gifs.png" />
-  </div>
+    <div class="gifs">
+        <div class="gifs__header">
+            <v-col cols="12" sm="12" md="12">
+                <v-text-field
+                    v-model="searchValue"
+                    @input="search(true)"
+                    label="Your search here"
+                    outlined
+                    dense
+                    autofocus
+                    append-icon="mdi-magnify"
+                ></v-text-field>
+            </v-col>
+        </div>
+        <div class="gifs__container">
+            <div class="gifs__container__list">
+                <div class="gifs__container__list__item" v-for="gif in gifs" :key="gif.id">
+                    <img :src="gif.images.fixed_width.url" :alt="gif.title" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import GiphyService from '@/services/giphy.service.js';
+import debounce from 'lodash.debounce';
+import Masonry from 'masonry-layout';
 
+export default {
+    data() {
+        return {
+            searchValue: '',
+            gifs: [],
+            pagination: null,
+            masonry: null,
+            giphyService: new GiphyService()
+        }
+    },
+    mounted() {
+        this.initInfiniteScroll();
+    },
+    methods: {
+        search: debounce(function (needReset = false) {
+            const offset = this.pagination ? this.pagination.offset + this.pagination.count + 1 : 0;
+            if(needReset) {
+                this.gifs = [];
+            }
+            this.giphyService.search(this.searchValue, offset)
+                .then(resp => JSON.parse(JSON.stringify(resp.data)))
+                .then(result => {
+                    this.gifs = [
+                        ...this.gifs,
+                        ...result.data,
+                    ];
+                    this.pagination = result.pagination;
+                    this.applyMasonry();
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+				
+        }, 500),
+        applyMasonry() {
+            const time2wait = this.pagination ? 500 : 1500;	
+            setTimeout(()=> {
+                this.masonry = new Masonry( document.querySelector('.gifs__container__list'), {
+                    itemSelector: '.gifs__container__list__item',
+                    transitionDuration: '20',
+                    percentPosition: true
+                });
+            }, time2wait);
+        },
+        initInfiniteScroll () {
+            const container = document.querySelector('.gifs__container');
+            container.onscroll = () => {
+                let bottomOfWindow = container.scrollTop === (container.scrollHeight - container.offsetHeight);
+                if (bottomOfWindow && this.pagination) {
+                    this.search();
+                }
+            };
+        },
+    }
 }
 </script>
 
 <style lang="scss">
-.fake {
-  max-width: 500px;
-  margin: auto;
-  img {
-    max-width: 100%;
-  }
+.gifs {
+    &__container {
+        height: calc(100vh - 200px);
+        overflow-y: auto;
+
+        &__list {
+            max-width: 650px;
+            margin: auto;
+
+            &__item {
+                width: calc(100% / 3);
+                padding: 5px;
+
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+
+    &__loader {
+        height: 100px;
+    }
 }
 </style>
