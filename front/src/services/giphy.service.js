f3b30fa0cf9754c8c1ac10f54066ba1088898cb3
Yann Tavernier <yann.tavernier@qima.com>
2020-03-27 07:37:59 +0100

    feat: refactor url

--- front/src/services/giphy.service.js
+++ front/src/services/giphy.service.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
-const baseUrlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.VUE_APP_GIPHY_API_KEY}`;
-const baseUrlRandom = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.VUE_APP_GIPHY_API_KEY}`;
+const baseUrl = `https://api.giphy.com/v1/gifs`;
 const type = 'gifs';
 
 export default class GiphyService  {
 
     search(q, offset = 0, limit = 17) {
-        const url = `${baseUrlSearch}&type=${type}&q=${q}&offset=${offset}&limit=${limit}`;
+        const url = this.getGiphyUrl(`/search?type=${type}&q=${q}&offset=${offset}&limit=${limit}`);
         return axios.get(url);
     }
 
     randomByTag(tag) {
-        const url = `${baseUrlRandom}&tag=${tag}`;
+        const url = this.getGiphyUrl(`/random?tag=${tag}`);
         return axios.get(url);
+    }
+
+    getGiphyUrl(request) {
+        return `${baseUrl}${request}&api_key=${process.env.VUE_APP_GIPHY_API_KEY}`
     }
 
 }
