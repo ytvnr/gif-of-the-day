f5b5baca1c1367c85790460cc7f69a430d68a9ae
Aurélien Loyer <aur.loy@gmail.com>
2020-04-17 14:11:43 +0200

    feat: display gif in full screen mode 🎉

---
+++ front/src/components/FullScreenGif.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="gif">
+        <v-icon class="gif__close" @click="$emit('close')">mdi-close</v-icon>
+        <img
+            class="gif__img"
+            :src="gif.images.original.url"
+            :alt="gif.title"
+            @load="isImageLoad = true"
+            :hidden="!isImageLoad"
+        />
+        <v-skeleton-loader v-if="!isImageLoad" type="image" class="gif__skeleton"></v-skeleton-loader>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isImageLoad: false,
+        }
+    },
+    props: ['gif']
+}
+</script>
+
+<style lang="scss" scoped>
+.gif {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: calc(100vh - 96px);
+    background: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__close {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        font-size: 40px;
+        padding: 20px;
+    }
+
+    &__img {
+        max-width: 100%;
+        max-height: calc(100vh - 200px);
+    }
+
+    &__skeleton {
+        width: 80%;
+    }
+}
+</style>
