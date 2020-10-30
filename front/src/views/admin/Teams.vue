0c879dcf923f5ac7201924f7ce6a9d4cf0c35531
Aurélien Loyer <aur.loy@gmail.com>
2020-10-31 00:38:06 +0100

    feat: add new team ☝️

--- front/src/views/admin/Teams.vue
+++ front/src/views/admin/Teams.vue
@@ -27,35 +27,31 @@
                         <v-card-text>
                             <v-container>
                                 <v-row>
-                                    <v-col cols="12" sm="6" md="4">
+                                    <v-col cols="12" sm="6" md="6">
+                                        <v-text-field
+                                            v-model="editedItem.avatar"
+                                            label="Avatar"
+                                            :rules="[rules.required]"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="6">
                                         <v-text-field
                                             v-model="editedItem.name"
-                                            label="Dessert name"
+                                            label="Name"
+                                            :rules="[rules.required]"
                                         ></v-text-field>
                                     </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field
-                                            v-model="editedItem.calories"
-                                            label="Calories"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field
-                                            v-model="editedItem.fat"
-                                            label="Fat (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field
-                                            v-model="editedItem.carbs"
-                                            label="Carbs (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field
-                                            v-model="editedItem.protein"
-                                            label="Protein (g)"
-                                        ></v-text-field>
+                                </v-row>
+                                <v-row>
+                                    <v-col cols="12" sm="12" md="12">
+                                        <v-select
+                                            :items="organizations"
+                                            v-model="editedItem.organization"
+                                            item-text="name"
+                                            item-value="id"
+                                            :rules="[rules.required]"
+                                            label="Select an organization"
+                                        />
                                     </v-col>
                                 </v-row>
                             </v-container>
@@ -66,33 +62,14 @@
                             <v-btn color="blue darken-1" text @click="close">
                                 Cancel
                             </v-btn>
-                            <v-btn color="blue darken-1" text @click="save">
+                            <v-btn
+                                color="blue darken-1"
+                                :disabled="!isIemValid"
+                                text
+                                @click="save"
+                            >
                                 Save
                             </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-                <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                        <v-card-title class="headline"
-                            >Are you sure you want to delete this
-                            item?</v-card-title
-                        >
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn
-                                color="blue darken-1"
-                                text
-                                @click="closeDelete"
-                                >Cancel</v-btn
-                            >
-                            <v-btn
-                                color="blue darken-1"
-                                text
-                                @click="deleteItemConfirm"
-                                >OK</v-btn
-                            >
-                            <v-spacer></v-spacer>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
@@ -101,9 +78,6 @@
         <template v-slot:item.actions="{ item }">
             <v-icon small class="mr-2" @click="editItem(item)">
                 mdi-pencil
-            </v-icon>
-            <v-icon small @click="deleteItem(item)">
-                mdi-delete
             </v-icon>
         </template>
         <template v-slot:no-data>
@@ -117,14 +91,15 @@
 import firebase from 'firebase';
 
 export default {
-    name: "organisations",
-    firestore: {
-        teams: firebase.firestore().collection('teams'),
-    },
+    name: "teams",
     data() {
         return {
+            teams: [],
+            organizations: [],
             dialog: false,
-            dialogDelete: false,
+            rules: {
+                required: value => !!value || 'Required.',
+            },
             headers: [
                 {
                     text: 'Avatar',
@@ -134,92 +109,100 @@ export default {
                 { text: 'Organization', value: 'organization.name' },
                 { text: 'Actions', value: 'actions', sortable: false },
             ],
-            teams: [],
-            editedIndex: -1,
+            editedId: null,
             editedItem: {
                 name: '',
                 avatar: '',
-                organisation: ''
+                organization: null
             },
             defaultItem: {
                 name: '',
                 avatar: '',
-                organisation: ''
+                organization: null
             },
         };
     },
     computed: {
+        isIemValid () {
+            return this.editedItem.avatar && this.editedItem.name && this.editedItem.organization; 
+        },
         formTitle () {
-            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+            return this.editedId === null ? 'New Item' : 'Edit Item'
         },
     },
 
     watch: {
         dialog (val) {
             val || this.close()
-        },
-        dialogDelete (val) {
-            val || this.closeDelete()
-        },
+        }
     },
 
-    created () {
-        this.initialize()
+    async created() {
+        await this.getOrganizations();
+        await this.getTeams();
     },
 
     methods: {
-        initialize () {
-            this.desserts = [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                }
-            ]
+
+        async getTeams() {
+            const teams = await this.concatDocument(firebase.firestore().collection('teams').get());
+            teams.forEach(team => {
+                const org = this.organizations.find(org => org.id === team.organization.id);
+                team.organization.name = org.name;
+            });
+            this.teams = teams;
+        },      
+        async getOrganizations() {
+            this.organizations = await this.concatDocument(firebase.firestore().collection('organizations').get());
+        },
+        async concatDocument(elem) {
+            return elem.then(querySnapshot => {
+                const documents = querySnapshot.docs.map(doc => {
+                    return {
+                        id: doc.id,
+                        ...doc.data(),
+                    }
+                })
+                return documents;
+            });
         },
 
         editItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedId = item.id;
             this.editedItem = Object.assign({}, item)
             this.dialog = true
-        },
-
-        deleteItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
-            this.editedItem = Object.assign({}, item)
-            this.dialogDelete = true
-        },
-
-        deleteItemConfirm () {
-            this.desserts.splice(this.editedIndex, 1)
-            this.closeDelete()
         },
 
         close () {
             this.dialog = false
             this.$nextTick(() => {
                 this.editedItem = Object.assign({}, this.defaultItem)
-                this.editedIndex = -1
-            })
-        },
-
-        closeDelete () {
-            this.dialogDelete = false
-            this.$nextTick(() => {
-                this.editedItem = Object.assign({}, this.defaultItem)
-                this.editedIndex = -1
+                this.editedId = null;
             })
         },
 
         save () {
-            if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem)
+            if (this.editedId) {
+                console.info('Edit team:', this.editedItem);
+                firebase.firestore().collection('teams')
+                    .doc(this.editedId)
+                    .update({
+                        ...this.editedItem,
+                        organization: firebase.firestore().collection('organizations').doc(this.editedItem.organization)
+                    })
+                    .then(() => {
+                        this.getTeams();
+                    })
             } else {
-                this.desserts.push(this.editedItem)
+                console.info('Add new team:', this.editedItem);
+                firebase.firestore().collection('teams').add({
+                    ...this.editedItem,
+                    organization: firebase.firestore().collection('organizations').doc(this.editedItem.organization)
+                }).then(() => {
+                    this.getTeams();
+                })
             }
-            this.close()
+            this.close();
         },
     },
 }
